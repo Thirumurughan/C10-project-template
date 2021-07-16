1f406152ab12500c2483204a571a4ae2ad7bab6e
Thirumurughan <83527123+Thirumurughan@users.noreply.github.com>
2021-07-16 22:22:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var btn_red;
 var btn_green;
+var red_bg
+var green_bg
 
 var r = 0;
 var g = 0;
@@ -7,7 +9,30 @@ var b = 0;
 
 function setup() {
   createCanvas(400, 400);  
+
+btn_red=createButton("RED");
+btn_red.position(100,50);
+btn_red.mousePressed(red_bg);
+
+btn_green=createButton("GREEN");
+btn_green.position(250,50);
+btn_green.mousePressed(green_bg);
 }
+
+function red_bg()
+{
+r=255;
+g=0;
+b=0;
+}
+
+function green_bg()
+{
+r=0;
+g=255;
+b=0;
+}
+
 
 function draw() {
   background(r,g,b);
